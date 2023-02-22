eb455e73a5da9ffb512b34d82edce80851dddb50
ditod1 <orlandodiazconde@gmail.com>
2023-02-22 14:49:47 -0500

    Add onclick button functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,66 +1,85 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+  }
+
+  handleBtnClick(e) {
+    this.setState((previousState) => calculate(previousState, e.target.innerText));
+  }
+
   render() {
     return (
       <div id="calculator" className="container bg-secondary">
-        <div className="text-end py-2">result</div>
+        <div className="text-end py-2">
+          {this.state.total}
+          {this.state.operation}
+          {this.state.next}
+        </div>
         <div className="row row-cols-4 text-center">
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             AC
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             +/-
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             %
           </button>
-          <button type="button" className="btn-calculator operator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator operator">
             ÷
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             7
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             8
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             9
           </button>
-          <button type="button" className="btn-calculator operator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator operator">
             x
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             4
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             5
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             6
           </button>
-          <button type="button" className="btn-calculator operator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator operator">
             -
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             1
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             2
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             3
           </button>
-          <button type="button" className="btn-calculator operator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator operator">
             +
           </button>
-          <button type="button" className="btn-calculator col-6">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator col-6">
             0
           </button>
-          <button type="button" className="btn-calculator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator">
             .
           </button>
-          <button type="button" className="btn-calculator operator">
+          <button onClick={this.handleBtnClick} type="button" className="btn-calculator operator">
             =
           </button>
         </div>
